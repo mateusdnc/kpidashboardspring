5de930f1b7c1a8b012aad618c966a234077a0639
mateusdnc <mateus.dias029@gmail.com>
2021-12-05 11:23:35 -0300

    separate js script from chart page

--- src/main/webapp/views/kpi/js/inflationBra.js
+++ src/main/webapp/views/kpi/js/inflationBra.js
@@ -1,31 +1,3 @@
-<html>
-
-<head>
-
-<meta charset="UTF-8">
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="/views/kpi/js/inflationBra"></script>
-
-</head>
-
-<style>
-.myChartdiv {
-	background-color: #3c4c66;
-	border-width: 50;
-	padding: 20;
-	border-radius: 10px;
-	border-color: darkblue;
-}
-</style>
-
-<div>
-	<canvas id="myChart"></canvas>
-</div>
-
-<script>
 	var inflationLabels = [];
 	var inflationBRAData = [];
 
@@ -92,6 +64,3 @@
 		var myChart = new Chart(document.getElementById('myChart'), config);
 
 	}
-</script>
-
-</html>
